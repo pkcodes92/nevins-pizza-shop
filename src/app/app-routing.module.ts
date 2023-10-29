19f74936604fcaf198769efc52ab45c73ff8e241
Pranav Krishnamurthy <pranav.krishnamurthy@emovis.us>
2023-10-29 10:47:29 -0400

    Making sure to have the necessary routes established

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from './home/home.component';
+import { ConfigComponent } from './config/config.component';
+import { OrderComponent } from './order/order.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'config', component: ConfigComponent },
+    { path: 'order', component: OrderComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ConfigComponent } from './config/config.component';
+import { OrderComponent } from './order/order.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ConfigComponent,
+    OrderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
