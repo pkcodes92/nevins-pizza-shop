ad04be67ceae6314be9a0b49b9ba38efbbc20d09
pkcodes92 <pranav.krishnamurthy@emovis.com>
2024-01-04 17:45:09 -0500

    PSU-1 Ensuring to have the environment setup

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private apiUrl = environment.apiUrl + 'api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
