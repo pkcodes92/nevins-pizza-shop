e4a64d388faa4ca5c31887eeaf4240607bc5e36e
pkcodes92 <pranav.krishnamurthy@emovis.com>
2024-01-04 21:00:16 -0500

    PSU-1 Ensuring to have the first add method implemented

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
+import { AddPizzaSauceRequest } from '../models/request';
+import { Observable } from 'rxjs';
+import { AddPizzaSauceResponse } from '../models/response';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +12,8 @@ export class ApiService {
   private apiUrl = environment.apiUrl + 'api';
 
   constructor(private http: HttpClient) { }
+
+  addSauce(addSauceRequest: AddPizzaSauceRequest): Observable<AddPizzaSauceResponse> {
+    return this.http.post<AddPizzaSauceResponse>(this.apiUrl + '/PizzaSauce/AddPizzaSauce', JSON.stringify(addSauceRequest));
+  }
 }
