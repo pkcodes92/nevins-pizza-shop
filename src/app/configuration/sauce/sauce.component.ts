fbb4830b14af5e9882deb7a582dd57e1d51e8737
pkcodes92 <pranav.krishnamurthy@emovis.com>
2024-01-09 18:08:40 -0500

    PSU-1 Making sure to have all of the methods wired up correctly

--- src/app/configuration/sauce/sauce.component.ts
+++ src/app/configuration/sauce/sauce.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { Subscription } from 'rxjs';
 import { PizzaSauce } from 'src/app/models/dto';
+import { AddPizzaSauceRequest, UpdatePizzaSauceRequest } from 'src/app/models/request';
 import { ApiService } from 'src/app/services/api.service';
 
 @Component({
@@ -11,7 +12,9 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrl: './sauce.component.scss'
 })
 export class SauceComponent implements OnInit, OnDestroy {
-  loading!: boolean;
+  loading: boolean = false;
+  editMode: boolean = true;
+
   pizzaSauces!: PizzaSauce[];
   addEditSauceForm!: FormGroup;
 
@@ -19,7 +22,7 @@ export class SauceComponent implements OnInit, OnDestroy {
   deleteSauceSubscription!: Subscription;
   getSaucesSubscription!: Subscription;
   getSauceSubscription!: Subscription;
-  updateSubscription!: Subscription;
+  updateSauceSubscription!: Subscription;
 
   constructor(private apiService: ApiService, private toastService: ToastrService) {
     
@@ -47,8 +50,8 @@ export class SauceComponent implements OnInit, OnDestroy {
         this.getSauceSubscription.unsubscribe();
       }
 
-      if (this.updateSubscription) {
-        this.updateSubscription.unsubscribe();
+      if (this.updateSauceSubscription) {
+        this.updateSauceSubscription.unsubscribe();
       }
   }
 
@@ -66,6 +69,7 @@ export class SauceComponent implements OnInit, OnDestroy {
       next: (response) => {
         if (response.statusCode === 200) {
           this.toastService.success('Got all of the sauces from the database', response.statusCode.toString());
+          this.pizzaSauces = response.pizzaSauces;
         }
 
         this.loading = false;
@@ -75,6 +79,15 @@ export class SauceComponent implements OnInit, OnDestroy {
         this.loading = false;
       }
     })
+  }
+
+  private determineEditMode(request: AddPizzaSauceRequest) {
+    var pizzaSauce = this.pizzaSauces.find(x => x.code == request.code);
+    if (pizzaSauce !== undefined) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   clearSauceForm() {
@@ -102,6 +115,54 @@ export class SauceComponent implements OnInit, OnDestroy {
   }
 
   onAddEditSauceFormSubmit() {
-    console.log('Making sure to have things determining whether to add or edit');
+    this.loading = true;
+
+    if (this.addEditSauceForm != null) {
+      this.editMode = this.determineEditMode(this.addEditSauceForm.value);
+
+      if (!this.editMode) {
+        let addSauceRequest = this.addEditSauceForm.value as AddPizzaSauceRequest;
+
+        addSauceRequest.appName = "Pizza Shop UI";
+
+        this.addEditSauceForm.controls['id'].disable();
+
+        this.addSauceSubscription = this.apiService.addSauce(addSauceRequest).subscribe({
+          next: (response) => {
+            if (response.statusCode === 201) {
+              this.toastService.success(`Successfully added the new sauce: ${addSauceRequest.code}`, response.statusCode.toString());
+              location.reload();
+            }
+
+            this.loading = false;
+          },
+          error: (error) => {
+            this.toastService.error(`Error when adding the new sauce: ${addSauceRequest.code}`, error.status.toString());
+            this.loading = false;
+          }
+        })
+      } else {
+        let updateSauceRequest = this.addEditSauceForm.value as UpdatePizzaSauceRequest;
+
+        updateSauceRequest.appName = "Pizza Shop UI";
+
+        this.addEditSauceForm.controls['id'].disable();
+
+        this.updateSauceSubscription = this.apiService.updateSauce(updateSauceRequest).subscribe({
+          next: (response) => {
+            if (response.statusCode === 200) {
+              this.toastService.success(`Successfully updated the sauce: ${updateSauceRequest.code}`, response.statusCode.toString());
+              location.reload();
+            }
+
+            this.loading = false;
+          },
+          error: (error) => {
+            this.toastService.error(`Error occurred updating the sauce: ${updateSauceRequest.code}`, error.status.toString());
+            this.loading = false;
+          }
+        });
+      }
+    }
   }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
-import { AddPizzaSauceRequest } from '../models/request';
+import { AddPizzaSauceRequest, UpdatePizzaSauceRequest } from '../models/request';
 import { Observable } from 'rxjs';
-import { AddPizzaSauceResponse, GetPizzaSaucesResponse } from '../models/response';
+import { AddPizzaSauceResponse, GetPizzaSauceResponse, GetPizzaSaucesResponse, UpdatePizzaSauceResponse } from '../models/response';
 
 @Injectable({
   providedIn: 'root'
@@ -15,13 +15,25 @@ export class ApiService {
 
   // #region Adding entities
   addSauce(addSauceRequest: AddPizzaSauceRequest): Observable<AddPizzaSauceResponse> {
-    return this.http.post<AddPizzaSauceResponse>(this.apiUrl + '/PizzaSauce/AddPizzaSauce', JSON.stringify(addSauceRequest));
+    let reqHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+    return this.http.post<AddPizzaSauceResponse>(this.apiUrl + '/PizzaSauce/AddPizzaSauce', JSON.stringify(addSauceRequest), {headers: reqHeaders});
   }
   // #endregion
 
   // #region Getting entities
   getAllSauces(): Observable<GetPizzaSaucesResponse> {
     return this.http.get<GetPizzaSaucesResponse>(this.apiUrl + '/PizzaSauce/GetAllSauces');
+  }
+
+  getPizzaSauce(id: number): Observable<GetPizzaSauceResponse> {
+    return this.http.get<GetPizzaSauceResponse>(this.apiUrl + `/PizzaSauce/GetPizzaSauceById?id=${id}`);
+  }
+  // #endregion
+
+  // #region Updating entities
+  updateSauce(updateSauceRequest: UpdatePizzaSauceRequest): Observable<UpdatePizzaSauceResponse> {
+    let reqHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+    return this.http.put<UpdatePizzaSauceResponse>(this.apiUrl + '/PizzSauce/UpdatePizzaSauce', JSON.stringify(updateSauceRequest), { headers: reqHeaders});
   }
   // #endregion
 }
