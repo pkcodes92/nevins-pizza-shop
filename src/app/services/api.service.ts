5823617555d0181154a414cab73fc09a532cb011
pkcodes92 <pranav.krishnamurthy@emovis.com>
2024-01-10 19:28:16 -0500

    PSU-1 Undoing the formatting of the api service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,72 +1,45 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
-import {
-  AddPizzaSauceRequest,
-  UpdatePizzaSauceRequest,
-} from '../models/request';
+import { AddPizzaSauceRequest, UpdatePizzaSauceRequest } from '../models/request';
 import { Observable } from 'rxjs';
-import {
-  AddPizzaSauceResponse,
-  DeletePizzaSauceResponse,
-  GetPizzaSauceResponse,
-  GetPizzaSaucesResponse,
-  UpdatePizzaSauceResponse,
-} from '../models/response';
+import { AddPizzaSauceResponse, DeletePizzaSauceResponse, GetPizzaSauceResponse, GetPizzaSaucesResponse, UpdatePizzaSauceResponse } from '../models/response';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class ApiService {
   private apiUrl = environment.apiUrl + 'api';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   // #region Adding entities
-  addSauce(
-    addSauceRequest: AddPizzaSauceRequest
-  ): Observable<AddPizzaSauceResponse> {
-    const reqHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.post<AddPizzaSauceResponse>(
-      this.apiUrl + '/PizzaSauce/AddPizzaSauce',
-      JSON.stringify(addSauceRequest),
-      { headers: reqHeaders }
-    );
+  addSauce(addSauceRequest: AddPizzaSauceRequest): Observable<AddPizzaSauceResponse> {
+    const reqHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+    return this.http.post<AddPizzaSauceResponse>(this.apiUrl + '/PizzaSauce/AddPizzaSauce', JSON.stringify(addSauceRequest), {headers: reqHeaders});
   }
   // #endregion
 
   // #region Getting entities
   getAllSauces(): Observable<GetPizzaSaucesResponse> {
-    return this.http.get<GetPizzaSaucesResponse>(
-      this.apiUrl + '/PizzaSauce/GetAllSauces'
-    );
+    return this.http.get<GetPizzaSaucesResponse>(this.apiUrl + '/PizzaSauce/GetAllSauces');
   }
 
   getPizzaSauce(id: number): Observable<GetPizzaSauceResponse> {
-    return this.http.get<GetPizzaSauceResponse>(
-      this.apiUrl + `/PizzaSauce/GetPizzaSauceById?id=${id}`
-    );
+    return this.http.get<GetPizzaSauceResponse>(this.apiUrl + `/PizzaSauce/GetPizzaSauceById?id=${id}`);
   }
   // #endregion
 
   // #region Updating entities
-  updateSauce(
-    updateSauceRequest: UpdatePizzaSauceRequest
-  ): Observable<UpdatePizzaSauceResponse> {
-    const reqHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.put<UpdatePizzaSauceResponse>(
-      this.apiUrl + '/PizzSauce/UpdatePizzaSauce',
-      JSON.stringify(updateSauceRequest),
-      { headers: reqHeaders }
-    );
+  updateSauce(updateSauceRequest: UpdatePizzaSauceRequest): Observable<UpdatePizzaSauceResponse> {
+    const reqHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+    return this.http.put<UpdatePizzaSauceResponse>(this.apiUrl + '/PizzSauce/UpdatePizzaSauce', JSON.stringify(updateSauceRequest), { headers: reqHeaders});
   }
   // #endregion
 
   // #region Deleting entities
   deleteSauce(id: number): Observable<DeletePizzaSauceResponse> {
-    return this.http.delete<DeletePizzaSauceResponse>(
-      this.apiUrl + `/PizzaSauce/DeleteSauce?id=${id}`
-    );
+    return this.http.delete<DeletePizzaSauceResponse>(this.apiUrl + `/PizzaSauce/DeleteSauce?id=${id}`);
   }
   // #endregion
 }
