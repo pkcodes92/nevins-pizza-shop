1000c120f3610e3b7254977b5e487358e83f49b0
Pranav Krishnamurthy <pkrish19@outlook.com>
2024-06-01 15:56:18 -0400

    Making sure to have all of the various updates done

--- src/app/configuration/configuration.module.ts
+++ src/app/configuration/configuration.module.ts
@@ -10,26 +10,20 @@ import { CrustComponent } from './crust/crust.component';
 import { CheeseComponent } from './cheese/cheese.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ApiService } from '../services/api.service';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-@NgModule({
-  declarations: [
-      ConfigComponent,
-      ToppingTypeComponent,
-      ToppingComponent,
-      SauceComponent,
-      PizzaSizeComponent,
-      CrustComponent,
-      CheeseComponent
-    ],
-  imports: [
-    ConfigurationRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    CommonModule
-  ],
-  providers: [
-    ApiService
-  ]
-})
+@NgModule({ declarations: [
+        ConfigComponent,
+        ToppingTypeComponent,
+        ToppingComponent,
+        SauceComponent,
+        PizzaSizeComponent,
+        CrustComponent,
+        CheeseComponent
+    ], imports: [ConfigurationRoutingModule,
+        ReactiveFormsModule,
+        CommonModule], providers: [
+        ApiService,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class ConfigurationModule { }
